3f69fb8c6bab3c20a5cd8b6d80e358a258235933
Bruce Cutler <bruce@sumebrius.net>
2024-05-14 21:33:02 +1200

    Better sidebar and input item

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,9 +1,18 @@
 <script setup>
 import NavMenuItem from './NavMenuItem.vue'
+import NavMenuInput from './NavMenuInput.vue'
 </script>
 
 <template>
-  <ul class="nav flex-sm-column w-25">
-    <NavMenuItem href="/foobar" title="First Item" />
-  </ul>
+  <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-primary-subtle text-primary-emphasis">
+    <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100">
+      <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-decoration-none">
+        <span class="fs-5 d-none d-sm-inline">Vapid</span>
+      </a>
+      <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start">
+        <NavMenuInput buttonIcon="bi-bar-chart-steps" placeholder="Spec Location" />
+      </ul>
+      <hr />
+    </div>
+  </div>
 </template>
---
+++ src/components/NavMenuInput.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(['buttonIcon', 'placeholder'])
+</script>
+
+<template>
+  <form class="d-flex">
+    <button class="btn btn-outline-primary me-2" type="button"><i :class="buttonIcon"></i></button>
+    <input type="text" class="form-control" :placeholder />
+  </form>
+</template>
