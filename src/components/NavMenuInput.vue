f0973fbd4151aa215988dda4c92cf0afce05cd18
Bruce Cutler <bruce@sumebrius.net>
2024-05-14 23:31:22 +1200

    Input event emission

--- src/components/NavMenuInput.vue
+++ src/components/NavMenuInput.vue
@@ -1,10 +1,23 @@
 <script setup>
+import { ref } from 'vue'
+
 defineProps(['buttonIcon', 'placeholder'])
+const emit = defineEmits(['submit'])
+
+const input = ref(null)
+
+function submit() {
+  if (input.value) {
+    emit('submit', input.value.value)
+  }
+}
 </script>
 
 <template>
-  <form class="d-flex w-100">
-    <button class="btn btn-outline-primary me-2" type="button"><i :class="buttonIcon"></i></button>
-    <input type="text" class="form-control" :placeholder />
+  <form class="d-flex w-100" @submit="submit">
+    <button class="btn btn-outline-primary me-2" type="button" @click="submit">
+      <i :class="buttonIcon"></i>
+    </button>
+    <input type="text" class="form-control" :placeholder ref="input" />
   </form>
 </template>
