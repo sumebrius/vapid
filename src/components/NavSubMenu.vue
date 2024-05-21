2adabf8333d0344fed5eca3db453d8517cd4e8ac
Bruce Cutler <bruce@sumebrius.net>
2024-05-22 00:21:43 +1200

    Allow custom click event callbacks for submenu items

--- src/components/NavSubMenu.vue
+++ src/components/NavSubMenu.vue
@@ -1,17 +1,18 @@
 <script setup>
 import { strToId } from '@/util'
 
-defineProps(['children'])
+const props = defineProps(['children', 'callback'])
+
+const callback = props.callback === undefined ? scrollTo : props.callback
 
 function scrollTo(item) {
-  console.log(item, strToId(item))
   document.getElementById(strToId(item)).scrollIntoView()
 }
 </script>
 
 <template>
   <ul class="nav flex-column ms-1">
-    <li class="w-100 ms-4" v-for="title in children" :key="title" @click.stop="scrollTo(title)">
+    <li class="w-100 ms-4" v-for="title in children" :key="title" @click.stop="callback(title)">
       <a class="nav-link px-0" role="button">
         <span class="d-none d-sm-inline">{{ title }}</span>
       </a>
