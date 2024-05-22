ee2916dd74c989904128e0a1b3bda7956887074a
Bruce Cutler <bruce@sumebrius.net>
2024-05-22 23:31:03 +1200

    TS tooling

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,13 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  plugins: ['@typescript-eslint'],
   parserOptions: {
     ecmaVersion: 'latest'
   }
